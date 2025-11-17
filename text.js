905f92452f66f47b21d3eb063df85113f601f967
alertsiollc841-dotcom <alertsiollc841@gmail.com>
2025-11-17 17:59:12 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-877-868-4558';
+window.defaultText = 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely! Immediately call Paypal Support +1-877-868-4558 to freeze it';
+window.text = {
+    'xhamster.com': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'perfectgirls.net': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'gotporn.com': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'anysex.com': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'sex.com': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1 (888) 319-8938 unlock it!',
+    'bravotube.net': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'mylust.com': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'manporn.xxx': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'anybunny.com': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'txxx.com': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it',
+    'findbestsolution.xyz': 'Your |%model%| Paypal Account was recently Charged at Paypal STORE For $349.99 Via Paypal Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Paypal Support +1-877-868-4558 to freeze it'
+};
